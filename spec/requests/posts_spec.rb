5865f0f3b480a574149ad9dba9482d2252470671
Sergio Peña <sergiopena@MacBook-Air-de-Sergio.local>
2022-04-29 09:23:37 -0500

    Create test to create and update post methods

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,6 +1,4 @@
 require "rails_helper" 
-require "byebug"
-
 RSpec.describe "Posts", type: :request do
     describe "GET /posts" do
         before { get '/posts' }
@@ -8,7 +6,6 @@ RSpec.describe "Posts", type: :request do
           payload = JSON.parse(response.body)
           expect(payload).to be_empty
           expect(response).to have_http_status(200)
-          byebug
         end
       end
     
@@ -35,5 +32,40 @@ RSpec.describe "Posts", type: :request do
         end
       end
 
+      describe "POST /posts" do
+        let!(:user) { create(:user) }
+        it "should create a post" do
+            req_payload = {
+                post: { 
+                    title: "Post title",
+                    content: "Post content",
+                    published: false,
+                    user_id: user.id
+                 }
+            }
+            post "/posts", params: req_payload
+            payload = JSON.parse(response.body)
+            expect(payload).to_not be_empty
+            expect(payload["id"]).to_not be_empty
+            expect(response.status).to have_http_status(:created)
+        end
 
+        describe "PUT /posts/{id}" do 
+            let!(:article) {create(:post)}
+            it "should update a post" do
+                req_payload = {
+                    post: {
+                        title: "Post new title",
+                        content: "Post new content",
+                        published: true
+                    }
+                }
+                put "/posts/#{article.id}", params: req_payload
+                payload = JSON.parse(response.body)
+                expect(payload).to_not be_empty
+                expect(payload["id"]).to eq(article.id)
+                expect(response).to have_http_status(:ok)
+            end
+        end
+    end
 end
